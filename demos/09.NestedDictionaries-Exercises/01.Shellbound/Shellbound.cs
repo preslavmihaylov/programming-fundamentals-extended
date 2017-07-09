4139669ef14a14905a899e596fa71cc6a0c2808d
PreslavMihaylov <pmihaylov95@gmail.com>
2017-07-09 08:57:16 +0300

    Added exercises for lecture 09

---
+++ demos/09.NestedDictionaries-Exercises/01.Shellbound/Shellbound.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Shellbound
+{
+    class Shellbound
+    {
+        static void Main()
+        {
+            Dictionary<string, HashSet<int>> regionsData = 
+                new Dictionary<string, HashSet<int>>();
+
+            string[] inputTokens = Console.ReadLine().Split(' ');
+
+            while (inputTokens[0] != "Aggregate")
+            {
+                string regionName = inputTokens[0];
+                int shell = int.Parse(inputTokens[1]);
+
+                if (!regionsData.ContainsKey(regionName))
+                {
+                    regionsData.Add(regionName, new HashSet<int>());
+                }
+
+                regionsData[regionName].Add(shell);
+                inputTokens = Console.ReadLine().Split(' ');
+            }
+
+            foreach (var regionData in regionsData)
+            {
+                string regionName = regionData.Key;
+                HashSet<int> shellsData = regionData.Value;
+
+                int avg = (int)shellsData.Average();
+                int sum = (int)shellsData.Sum();
+                int giantShellSize = sum - avg;
+
+                Console.WriteLine("{0} -> {1} ({2})", 
+                    regionName,
+                    string.Join(", ", shellsData),
+                    giantShellSize);
+            }
+        }
+    }
+}
