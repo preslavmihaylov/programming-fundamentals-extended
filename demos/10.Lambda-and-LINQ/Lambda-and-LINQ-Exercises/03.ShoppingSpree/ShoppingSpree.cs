829f82dcb2576832bbf11d4dcfb3e3e6488b6327
PreslavMihaylov <pmihaylov95@gmail.com>
2017-07-16 08:58:52 +0300

    added exercise linq-more-exercises

---
+++ demos/10.Lambda-and-LINQ/Lambda-and-LINQ-Exercises/03.ShoppingSpree/ShoppingSpree.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.ShoppingSpree
+{
+    class ShoppingSpree
+    {
+        static void Main()
+        {
+            var data = new Dictionary<string, double>();
+
+            double budget = double.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+            while (input != "end")
+            {
+                string[] inputTokens =
+                    input.Split(' ');
+
+                string product = inputTokens[0];
+                double price = double.Parse(inputTokens[1]);
+
+                if (data.ContainsKey(product))
+                {
+                    if (data[product] > price)
+                    {
+                        data[product] = price;
+                    }
+                }
+                else
+                {
+                    data[product] = price;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            double totalPrice = data.Sum(r => r.Value);
+
+            if (totalPrice > budget)
+            {
+                Console.WriteLine("Need more money... Just buy banichka");
+            }
+            else
+            {
+                var orderedData = data
+                    .OrderByDescending(r => r.Value)
+                    .ThenBy(r => r.Key.Length);
+
+                foreach (var item in orderedData)
+                {
+                    Console.WriteLine("{0} costs {1:F2}", item.Key, item.Value);
+                }
+            }
+        }
+    }
+}
---
+++ demos/10.Lambda-and-LINQ/Lambda-and-LINQ-Exercises/05.FlattenDictionary/FlattenDictionary.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.FlattenDictionary
+{
+    class InnerValue
+    {
+        public string value;
+        public bool flattened;
+
+        public InnerValue(string value, bool flattened)
+        {
+            this.value = value;
+            this.flattened = flattened;
+        }
+    }
+
+    class FlattenDictionary
+    {
+        static void Main()
+        {
+            var data = new Dictionary<string, Dictionary<string, string>>();
+
+            string input = Console.ReadLine();
+            while (input != "end")
+            {
+                string[] inputTokens =
+                    input.Split(' ');
+
+                if (inputTokens[0] != "flatten")
+                {
+                    string key = inputTokens[0];
+                    string innerKey = inputTokens[1];
+                    string value = inputTokens[2];
+
+                    if (!data.ContainsKey(key))
+                    {
+                        data.Add(key, new Dictionary<string, string>());
+                    }
+
+                    data[key][innerKey] = value;
+                }
+                else
+                {
+                    string keyToFlatten = inputTokens[1];
+
+                    data[keyToFlatten] = 
+                        data[keyToFlatten].ToDictionary(r => r.Key + r.Value, r => "flattened");
+                }
+
+                input = Console.ReadLine();
+            }
+
+            var orderedData = data
+                .OrderByDescending(r => r.Key.Length)
+                .ToDictionary(r => r.Key, r => r.Value);
+
+            foreach (var record in orderedData)
+            {
+                string key = record.Key;
+                Dictionary<string, string> innerRecords = record.Value;
+
+                Console.WriteLine(key);
+                var unflattenedRecords = innerRecords
+                    .Where(r => r.Value != "flattened")
+                    .OrderBy(r => r.Key.Length);
+
+                var flattenedRecords = innerRecords
+                    .Where(r => r.Value == "flattened");
+
+                int id = 1;
+                foreach (var unflattenedRecord in unflattenedRecords)
+                {
+                    Console.WriteLine("{0}. {1} - {2}",
+                        id++,
+                        unflattenedRecord.Key,
+                        unflattenedRecord.Value);
+                }
+
+                foreach (var flattenedRecord in flattenedRecords)
+                {
+                    Console.WriteLine("{0}. {1}",
+                        id++,
+                        flattenedRecord.Key);
+                }
+            }
+        }
+    }
+}
